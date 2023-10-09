fd9fcf1fb11644a90ca7579babc1897cf45306f3
viatovegas <145833157+viatovegas@users.noreply.github.com>
2023-10-09 16:15:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-// JavaScript
-console.log('Hello world!');
+// 定数の定義
+const loading = document.getElementById('loading');
+
+// cssクラスを追加する関数
+function animation(){
+  loading.classList.add('loaded');
+}
+
+// 指定秒後に動作させる
+window.setTimeout( animation, 2000 );
